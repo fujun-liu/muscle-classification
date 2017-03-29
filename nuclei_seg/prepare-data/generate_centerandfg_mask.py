b2f1a9a3962d1ef72f5440e383dc22f6b7229c90
Fujun Liu <liufujun07@gmail.com>
2017-03-29 18:44:03 -0400

    generate samples

--- nuclei_seg/prepare-data/generate_centerandfg_mask.py
+++ nuclei_seg/prepare-data/generate_centerandfg_mask.py
@@ -7,89 +7,39 @@ from PIL import Image, ImageDraw
 import matplotlib.pyplot as plt
 import random
 import cv2
-import h5py
 from skimage.measure import regionprops
 
-def extimate_radius(contour):
-	diameter = .0
-	num_pts = contour.shape[1]
-	for i in range(num_pts-1):
-		for j in range(i+1, num_pts):
-			diameter = max(diameter, np.linalg.norm(contour[:,i] - contour[:,j]))
-	return diameter/2.0
-			
-def generate_masks(img_dir, img_ext, gt_dir, gt_ext, **kwargs):
-	'''
-	This function generate the contour and foreground masks, taking the image its contour points as input
-	'''
-	fixed_dis = 3.0
-	radius_ratio = 0.5
-	min_dis = 1.5
-	sigma = 5
-	img_paths = glob.glob(img_dir + '/*' + img_ext)
-	for img_path in img_paths:
-		print 'generate contour and foreground masks for {}'.format(img_path)
-		img = np.asarray(Image.open(img_path))
-		img_name = os.path.splitext(os.path.split(img_path)[-1])[0]
-		contour_path = gt_dir + '/' + img_name + gt_ext
-		use_h5py = False
-		try:
-			xy = loadmat(contour_path)['Contours'][0]
-		except NotImplementedError:
-			# this is matlab -v7.3 file
-			f = h5py.File(contour_path)
-			xy = [f[element[0]][:] for element in f['Contours']]
-			use_h5py = True
-			print 'This is a -v7.3 file. Read it using h5py'
-		imgH, imgW, _ = img.shape
-		center_mask = np.zeros([imgH, imgW])
-		fg_mask = np.zeros_like(center_mask)
-		edge_mask = np.zeros_like(center_mask)
-		weight_map = np.zeros_like(center_mask)
-		for i in range(len(xy)):
-			contour = xy[i]
-			if use_h5py:
-				contour = np.transpose(contour)
-			xs = [min(x, imgW-1) for x in contour[0]]
-			ys = [min(y, imgH-1) for y in contour[1]]
-			
-			# compute the polygon for this cell
-			polygon = zip(xs, ys)
-			cell_mask = Image.new('L', (imgW, imgH), 0)
-			ImageDraw.Draw(cell_mask).polygon(polygon, fill=1)
-			cell_mask = ndimage.binary_erosion(np.array(cell_mask, dtype=np.int), structure = np.ones((5,5)))
-			fg_mask += np.array(cell_mask)
-			edge_mask[ys, xs] = 1
-			props = regionprops(cell_mask.astype(np.int))
-			if fixed_dis > 0:
-				dis_thresh = fixed_dis
-			else:
-				radius = 0.5 * props[0].minor_axis_length
-				dis_thresh = max(min_dis, radius*radius_ratio)
-			
-			centers = props[0].centroid
-			cell_center_mask = np.zeros([imgH, imgW])
-			cell_center_mask[centers[0], centers[1]] = 1
-			dist = ndimage.distance_transform_edt(1 - cell_center_mask)
-			weight_map += np.exp(-dist**2/sigma**2)
-			center_mask += dist < dis_thresh 
-		weight_map[weight_map > 1.0] = 1.0
-		fg_mask[fg_mask > 0] = 1	
-		center_mask[center_mask > 0] = 1	
-		
-		edge_mask = cv2.dilate(edge_mask, np.ones((5,5),np.uint8), iterations=1)
-		edge_mask = fg_mask * edge_mask	
+def generate_masks():
+    
+    gt_paths = glob.glob(gt_dir + '/*' + gt_ext)
+    for gt_path in gt_paths:
+        print 'generate masks for {}'.format(gt_path)
+        img_name = os.path.split(gt_path)[-1][:-len(gt_ext)]
+        xy = np.asarray(loadmat(gt_path)['cents']).astype(int)
+        img_path = os.path.join(img_dir, img_name + img_ext)
+        img = np.asarray(cv2.imread(img_path))
+        imgH, imgW, _ = img.shape
+        center_mask = np.zeros([imgH, imgW])
+        center_mask[xy[:,1], xy[:,0]] = 1
+        print len(xy)
+        print  np.sum(center_mask)
+        dist = ndimage.distance_transform_edt(1 - center_mask)
+        weight_map = np.exp(-dist**2/sigma**2)
+        print np.max(weight_map)
+        dst_path = os.path.join(annot_weight_dir, img_name + '_weight.png')
+        cv2.imwrite(dst_path, weight_map*255)
+        dst_path = os.path.join(annot_weight_dir, img_name + '_annot.png')
+        cv2.imwrite(dst_path, center_mask*255)
+        
 
-		Image.fromarray((edge_mask*255).astype(np.uint8)).save(img_dir + '/' + img_name + '_edge.png', 'png')
-		Image.fromarray((fg_mask*255).astype(np.uint8)).save(img_dir + '/' + img_name + '_fg.png', 'png')
-		Image.fromarray((center_mask*255).astype(np.uint8)).save(img_dir + '/' + img_name + '_center.png', 'png')
-		Image.fromarray((weight_map*255).astype(np.uint8)).save(img_dir + '/' + img_name + '_weight.png', 'png')
+
+img_dir = '/media/fujunl/FujunLiu/muscle-classification/nuclei_annotation/Images'
+gt_dir = '/media/fujunl/FujunLiu/muscle-classification/nuclei_annotation/positiveCoords'
+annot_weight_dir = 'tmp'
+img_ext, gt_ext = '.bmp', '.mat'
+sigma = 3
 
 if __name__ == '__main__':
-	
-	img_dir = 'img-train'
-	gt_dir = 'img-train'
-	img_ext, gt_ext = '.tif', '_seg.mat'
-	generate_masks(img_dir, img_ext, gt_dir, gt_ext)
+	generate_masks()
 
 
--- nuclei_seg/prepare-data/generate_training_data_sample.py
+++ nuclei_seg/prepare-data/generate_training_data_sample.py
@@ -3,126 +3,88 @@ import glob,shutil
 import numpy as np
 import random
 from PIL import Image, ImageDraw
-#import cv2
+import cv2
 
-img_dir = 'img-train'
-center_mask_dir, fg_mask_dir, weight_map_dir = img_dir, img_dir, img_dir 
-img_ext, center_mask_ext, fg_mask_ext, weight_map_ext, edge_ext = '.tif', '_center.png', '_fg.png', '_weight.png', '_edge.png'
-patch_size = 128
-stride = 32
-train_ratio = 1.0
+img_dir, img_ext = '/media/fujunl/FujunLiu/muscle-classification/nuclei_annotation/Images', '.bmp'
+weight_dir, weight_ext = 'tmp', '_weight.png'
+annot_dir, annot_ext = 'tmp', '_annot.png'
+patch_size = 100
+stride = 50
 train_dir = 'train'
-test_dir = 'test'
-train_lst_file = 'train_pair.lst'
-test_lst_file = 'test_pair.lst'
-mean_file = 'mean.txt'
 
-def generate_training_data():
+def generate_training_data_sliding():
 	'''
-	Generate training samples for training
+	Generate training samples for training in sliding window way from a big Image
 	'''
-
-	img_paths = glob.glob(img_dir + '/*' + img_ext)
-	all_mean = np.zeros(3)
-	train_lst = []
-	test_lst = []
-	
-	R = patch_size/2
-	
+	center_paths = glob.glob(weight_dir + '/*' + weight_ext)
+	R = patch_size // 2
 	rot_angles = [0,1,2]
 	random.seed()
-	for img_path in img_paths:
-		print 'Generating patches from {}'.format(img_path)
-		# normalize data in the range [0,1], case image max valus exceeds 255
-		#img = 1.0*cv2.imread(img_path)
-		img = 1.0*np.array(Image.open(img_path))
-		img = img/max(255.0, 1.0*np.max(img))
-		imgH, imgW, _ = img.shape
-		all_mean += np.sum(np.sum(img,axis=0), axis=0)/(imgH*imgW)
-		# read center mask and foreground mask
-		img_name = os.path.splitext(os.path.split(img_path)[-1])[0]
-		#img_center_mask = cv2.imread(os.path.join(center_mask_dir, img_name + center_mask_ext))
-		img_center_mask = np.array(Image.open(os.path.join(center_mask_dir, img_name + center_mask_ext)))
-		if img_center_mask is None:
-			print 'Invalid center mask path {}'.format(os.path.join(center_mask_dir, img_name + center_mask_ext))
-			sys.exit(1)
-		#img_fg_mask = cv2.imread(os.path.join(fg_mask_dir, img_name + fg_mask_ext))
-		img_fg_mask = np.array(Image.open(os.path.join(fg_mask_dir, img_name + fg_mask_ext)))
-		if img_fg_mask is None:
-			print 'Invalid fg mask path {}'.format(os.path.join(fg_mask_dir, img_name + fg_mask_ext))
-			sys.exit(1)
-		img_edge_mask = np.array(Image.open(os.path.join(fg_mask_dir, img_name + edge_ext)))
-		
-		weight_map = np.array(Image.open(os.path.join(weight_map_dir, img_name + weight_map_ext)))
-		# get all patch coords
-		start_x = R + random.randint(0, stride-1)
-		start_y = R + random.randint(0, stride-1)
-		gridX, gridY = range(start_x,imgW-R,stride), range(start_y,imgH-R,stride)
+	for center_path in center_paths[:1]:
+	    print 'Generating patches from {}'.format(center_path)
 
-		if len(gridX) == 0 or len(gridY) == 0:
-			continue
-		[meshY, meshX] = np.meshgrid(gridY, gridX)
-		meshY = meshY.flatten()
-		meshX = meshX.flatten()
+        img_name = os.path.split(center_path)[-1][:-len(weight_ext)]
+        img_path = os.path.join(img_dir, img_name + img_ext)
+        weight_map = cv2.imread(center_path)
+        img = cv2.imread(img_path)
+        imgH, imgW = img.shape[:2]
+	    # get all patch coords
+        start_x = R + random.randint(0, stride-1)
+        start_y = R + random.randint(0, stride-1)
+        gridX, gridY = range(start_x, imgW-R, stride), range(start_y, imgH-R, stride)   
+        [meshY, meshX] = np.meshgrid(gridY, gridX)
+        meshY = meshY.flatten()
+        meshX = meshX.flatten() 
+        #print sample_indice
+        for iy, ix in zip(meshY, meshX):
+            patch_img = img[iy-R+1:iy+R+1, ix-R+1:ix+R+1,:]
+            patch_weight = weight_map[iy-R+1:iy+R+1, ix-R+1:ix+R+1]
+            for rotk in rot_angles: 
+                patch_img_rot = np.rot90(patch_img, rotk)
+                patch_weight_rot = np.rot90(patch_weight, rotk)
+                pid = '{}_{}_{}'.format(iy, ix, rotk)
+                patch_img_dst = os.path.join(train_dir, img_name + '_' + pid + '.png')
+                cv2.imwrite(patch_img_dst, patch_img_rot)
+                patch_weight_dst = os.path.join(train_dir, img_name + '_' + pid + '_weight.png')
+                cv2.imwrite(patch_weight_dst, patch_weight_rot)
 
-		#print sample_indice
-		for iy, ix in zip(meshY, meshX):
-			patch_img = img[iy-R+1:iy+R+1, ix-R+1:ix+R+1,:]
-			patch_center = img_center_mask[iy-R+1:iy+R+1, ix-R+1:ix+R+1]
-			patch_fg = img_fg_mask[iy-R+1:iy+R+1, ix-R+1:ix+R+1]
-			patch_edge = img_edge_mask[iy-R+1:iy+R+1, ix-R+1:ix+R+1]
-			patch_weight = weight_map[iy-R+1:iy+R+1, ix-R+1:ix+R+1]
-			for rotk in rot_angles: 
-				patch_img_rot = np.rot90(patch_img, rotk)
-				patch_center_rot = np.rot90(patch_center, rotk)
-				patch_fg_rot = np.rot90(patch_fg, rotk)
-				patch_edge_rot = np.rot90(patch_edge, rotk)
-				patch_weight_rot = np.rot90(patch_weight, rotk)
-				pid = '{}_{}_{}'.format(iy, ix, rotk)
-				if np.random.random() < train_ratio:
-					# for training
-					patch_img_dst = os.path.join(train_dir, img_name + '_' + pid + '.png')
-					patch_center_dst = os.path.join(train_dir, img_name + '_' + pid + '_center.png')
-					patch_fg_dst = os.path.join(train_dir, img_name + '_' + pid + '_fg.png')
-					patch_edge_dst = os.path.join(train_dir, img_name + '_' + pid + '_edge.png')
-					patch_weight_dst = os.path.join(train_dir, img_name + '_' + pid + '_weight.png')
-					train_lst.append((patch_img_dst, patch_center_dst,patch_fg_dst, patch_weight_dst, patch_edge_dst))
-				else:
-					# for testing
-					patch_img_dst = os.path.join(test_dir, img_name + '_' + pid + '.png')
-					patch_center_dst = os.path.join(test_dir, img_name + '_' + pid + '_center.png')
-					patch_fg_dst = os.path.join(test_dir, img_name + '_' + pid + '_fg.png')
-					patch_edge_dst = os.path.join(test_dir, img_name + '_' + pid + '_edge.png')
-					patch_weight_dst = os.path.join(test_dir, img_name + '_' + pid + '_weight.png')
-					test_lst.append((patch_img_dst, patch_center_dst, patch_fg_dst, patch_weight_dst, patch_edge_dst))
-				# save data into its folder
-				#cv2.imwrite(patch_img_dst, 255*patch_img_rot)	
-				Image.fromarray((255*patch_img_rot).astype(np.uint8)).save(patch_img_dst, 'png')
-				#cv2.imwrite(patch_center_dst, patch_center_rot)	
-				Image.fromarray((patch_center_rot).astype(np.uint8)).save(patch_center_dst, 'png')
-				#cv2.imwrite(patch_fg_dst, patch_fg_rot)	
-				Image.fromarray((patch_fg_rot).astype(np.uint8)).save(patch_fg_dst, 'png')
-				Image.fromarray((patch_edge_rot).astype(np.uint8)).save(patch_edge_dst, 'png')
-				Image.fromarray((patch_weight_rot).astype(np.uint8)).save(patch_weight_dst, 'png')
-	# write train list
-	with open(train_lst_file, 'w') as f:
-		for img_path, center_path, fg_path, weight_path, edge_path in train_lst:
-			f.write('{} {} {} {} {}\n'.format(img_path, center_path, fg_path, edge_path, weight_path))
-	# write test lst
-	with open(test_lst_file, 'w') as f:
-		for img_path, center_path, fg_path, weight_path, edge_path in test_lst:
-			f.write('{} {} {} {} {}\n'.format(img_path, center_path, fg_path, edge_path, weight_path))
-	# write mean to file
-	all_mean /= len(img_paths)
-	with open(mean_file, 'w') as f:
-		f.write('{} {} {}\n'.format(all_mean[0], all_mean[1], all_mean[2]))
-
+def generate_training_data_sampling():
+    	'''
+	Generate training samples centering around each centers
+	'''
+	center_paths = glob.glob(weight_dir + '/*' + weight_ext)
+	R = patch_size // 2
+	rot_angles = [0,1,2]
+	random.seed()
+	for center_path in center_paths[:1]:
+	    print 'Generating patches from {}'.format(center_path)
+        weight_map = cv2.imread(center_path)
+        img_name = os.path.split(center_path)[-1][:-len(weight_ext)]
+        # read Image
+        img_path = os.path.join(img_dir, img_name + img_ext)
+        img = cv2.imread(img_path)
+        # read dot annot
+        annot_path = os.path.join(annot_dir, img_name + annot_ext)
+        dot_annot = cv2.imread(annot_path)[:,:,1]
+        imgH, imgW = img.shape[:2]
+        #print sample_indice
+        for iy, ix in zip(*np.nonzero(dot_annot)):
+            left, top = ix-R, iy-R
+            if left < 0 or top < 0: continue
+            if left+patch_size > imgW or top+patch_size > imgH: continue
+            patch_img = img[top:top+patch_size, left:left+patch_size,:]
+            patch_weight = weight_map[top:top+patch_size, left:left+patch_size]
+            for rotk in rot_angles: 
+                patch_img_rot = np.rot90(patch_img, rotk)
+                patch_weight_rot = np.rot90(patch_weight, rotk)
+                pid = '{}_{}_{}'.format(iy, ix, rotk)
+                patch_img_dst = os.path.join(train_dir, img_name + '_' + pid + '.png')
+                cv2.imwrite(patch_img_dst, patch_img_rot)
+                patch_weight_dst = os.path.join(train_dir, img_name + '_' + pid + '_weight.png')
+                cv2.imwrite(patch_weight_dst, patch_weight_rot)
 
 if __name__ == '__main__':
-	if os.path.isdir(train_dir):
-		shutil.rmtree(train_dir)
-	if os.path.isdir(test_dir):
-		shutil.rmtree(test_dir)
-	os.mkdir(train_dir)
-	os.mkdir(test_dir)
-	generate_training_data()
+    if os.path.isdir(train_dir):
+    	shutil.rmtree(train_dir)
+    os.mkdir(train_dir)
+    generate_training_data_sampling()    
